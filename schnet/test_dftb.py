637d86ab4a3242ec48d90f49e51ccaab8adb8ee4
Piotr Wróbel <pwrobel5@gmail.com>
2023-10-01 00:12:09 +0500

    saving predicted charges in format accepted by DFTB+

--- schnet/test_dftb.py
+++ schnet/test_dftb.py
@@ -16,6 +16,67 @@ AA__BOHR = 1 / BOHR__AA
 CUTOFF = 7.5
 
 
+class ChargeWriter:
+
+    VALENCE_POPULATIONS_ATOMS = {
+        1: 1,
+        2: 2,
+        3: 1,
+        4: 2,
+        5: 3,
+        6: 4,
+        7: 5,
+        8: 6,
+        9: 7,
+        10: 8
+    }
+
+    HEADER = '6\n F F F T {} 1 {:.16f}\n'
+
+    def __init__(self, numbers, charges, charge_file_name='charges-test.dat'):
+        self._numbers = numbers
+        self._charges = charges
+        self._charge_file_name = charge_file_name
+    
+    def set_charges(self, charges):
+        self._charges = charges
+
+    def __correct_populations(self, valence_populations):
+        desired_sum = sum([ChargeWriter.VALENCE_POPULATIONS_ATOMS[i] for i in self._numbers])
+        current_sum = sum(valence_populations)
+        difference = desired_sum - current_sum
+        atom_weights = [ChargeWriter.VALENCE_POPULATIONS_ATOMS[i] / desired_sum for i in self._numbers]
+        
+        for i in range(0, len(valence_populations)):
+            valence_populations[i] += atom_weights[i] * difference
+    
+    def __get_valence_populations(self):
+        valence_populations = []
+
+        for number, charge in zip(self._numbers, self._charges):
+            population = ChargeWriter.VALENCE_POPULATIONS_ATOMS[number] - charge.item()
+            valence_populations.append(population)
+
+        self.__correct_populations(valence_populations)
+        return valence_populations
+
+    def save_charges(self):
+        valence_populations = self.__get_valence_populations()
+        charge_file = open(self._charge_file_name, 'w')
+
+        charge_file.write(ChargeWriter.HEADER.format(len(valence_populations), sum(valence_populations)))
+
+        for population, number in zip(valence_populations, self._numbers):
+            if number > 2:
+                charge_file.write('{:.16f} {:.16f} {:.16f} {:.16f}\n'.format(population, 0.0, 0.0, 0.0))
+            else:
+                charge_file.write('{:.16f}\n'.format(population))
+        
+        charge_file.write('0 0\n')
+
+        charge_file.close()
+
+
 def main():
     coords = np.array([
         [0.5988008852E+00,   -0.1668846477E+00,    0.2470279826E+00],
@@ -47,7 +108,12 @@ def main():
     
     model = torch.load(os.path.join(MODEL_PATH, 'best_inference_model'), map_location=torch.device('cpu'))
     prediction = model(inputs)
-    print('Prediction:', prediction['charges'])
+    charges = prediction['charges']
+
+    writer = ChargeWriter(numbers, charges)
+    writer.save_charges()
+
+    #print('Prediction:', prediction['charges'])
 
     #coords *= AA__BOHR
     #latvecs *= AA__BOHR
